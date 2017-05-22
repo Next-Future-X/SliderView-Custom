bd64c7beb3a13d402bdc0829d16170b18352b707
yuanxiangyang0528 <yuanxiangyang0528@163.com>
2017-05-22 14:50:17 +0800

    Initial Commit

---
+++ 自定义sliderView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  自定义sliderView
+//
+//  Created by 袁向阳 on 17/5/22.
+//  Copyright © 2017年 YXY.cn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
